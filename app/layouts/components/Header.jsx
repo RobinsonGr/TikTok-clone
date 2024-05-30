0b9fddc1e29a872cb3a6f376fe8026e89b9934ef
RobinsonGr <robinsonandresgr@gmail.com>
2024-05-29 23:46:39 -0500

    setting up the header with logo, search bar and buttons, just fosused in the UI side

---
+++ app/layouts/components/Header.jsx
@@ -0,0 +1,18 @@
+
+import Link from "next/link"
+
+//I will wrapp this in a client component, just for now, i will use use client
+
+export default function Header(){
+
+    return (
+        <>
+            <div className=""></div>
+
+            <Link href="/">
+                <img src="/tiktok-logo.png"/>
+            </Link>
+        
+        </>
+    )
+}
