985e9f7d36c9c3600e5c17b8fd345766b30f326b
RobinsonGr <robinsonandresgr@gmail.com>
2024-05-31 23:52:23 -0500

    finishing header

--- app/layouts/components/Header.jsx
+++ app/layouts/components/Header.jsx
@@ -19,19 +19,30 @@ export default function Header(){
                 </Link>
 
             {/*search bar */}
-            <div className=" flex justify-between items-center h-10 px-3 bg-gray-200 rounded-full">
-                <input className="bg-gray-200"/>
-                <BiSearch/>
+            <div className="max-w-md px-3 py-1 flex justify-between items-center h-10  bg-gray-200 rounded-full">
+                <input 
+                type="text"
+                placeholder="Search"
+                className="w-full my-2 pl-2 mr-2 text-gray-500 bg-gray-200 border-e-2 placeholder:text-gray-400 border-gray-300 focus:outline-none"/>
+                <BiSearch color="#A1A2A7" size="22"/>
             </div>
 
-            {/*right buttons upload and profile */}
-                <div>
-                    <div>Upload</div>
-                    <AiOutlinePlus/>
+            {/*right buttons */}
+                <div className="flex items-center gap-3">
 
-                    <Link href='/'>
-                        <img src="https://placehold.co/25"/>
-                    </Link>
+                    {/* + Upload button */}
+                    <button className="flex items-center px-3 py-2 border rounded-s hover:bg-gray-200">
+                        <AiOutlinePlus  color="#000000" size="22" />
+                        <span className="px-2 text-base font-medium">Upload</span>  
+                    </button>
+
+                    {/* + profile controls */}
+                    <button >
+                        <Link href='/'>
+                            <img src="https://placehold.co/25" className="rounded-full size-9"/>
+                        </Link>
+                    </button>
+                   
                 </div>
         </div>
         </div>
