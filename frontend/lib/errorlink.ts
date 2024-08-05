6ec69a52e892be7ede6ec1d888155fa92c6831a8
RobinsonGr <robinsonandresgr@gmail.com>
2024-08-04 23:58:00 -0500

    config erro catching and links apollo client separate them by modules

--- frontend/lib/errorlink.ts
+++ frontend/lib/errorlink.ts
@@ -1,36 +1,40 @@
-import { onError } from "@apollo/client/link/error";
-import { Observable } from "@apollo/client";
-import { refreshToken } from './refreshToken';
-import { getClient } from './apollo-client';
+// import { onError } from "@apollo/client/link/error";
+// import { Observable } from "@apollo/client";
+// import { refreshToken } from './refreshToken';
+// import { getClient } from './apollo-client';
 
-let retryCount = 0;
-const maxRetry = 3;
+// let retryCount = 0;
+// const maxRetry = 3;
 
-export const errorLink = onError(({ graphQLErrors, operation, forward }) => {
-  const operationName = operation.operationName;
-  console.log(operationName, "operationName");
+// export const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+//   const operationName = operation.operationName;
+//   console.log(operationName, "operationName");
 
-  if (graphQLErrors) {
-    for (const err of graphQLErrors) {
-      if (err.extensions.code === "UNAUTHENTICATED" && retryCount < maxRetry) {
-        retryCount++;
+//   if (graphQLErrors) {
+//     for (const err of graphQLErrors) {
+//       if (err.extensions.code === "UNAUTHENTICATED" && retryCount < maxRetry) {
+//         retryCount++;
 
-        return new Observable((observer) => {
-          refreshToken(getClient())
-            .then((token) => {
-              console.log("token", token);
-              operation.setContext((previousContext: any) => ({
-                headers: {
-                  ...previousContext.headers,
-                  authorization: token,
-                },
-              }));
-              const forward$ = forward(operation);
-              forward$.subscribe(observer);
-            })
-            .catch((error) => observer.error(error));
-        });
-      }
-    }
-  }
-});
+//         return new Observable((observer) => {
+//           refreshToken(getClient())
+//             .then((token) => {
+//               console.log("token", token);
+//               operation.setContext((previousContext: any) => ({
+//                 headers: {
+//                   ...previousContext.headers,
+//                   authorization: token,
+//                 },
+//               }));
+//               const forward$ = forward(operation);
+//               forward$.subscribe(observer);
+//             })
+//             .catch((error) => observer.error(error));
+//         });
+//       }
+//     }
+//   }
+// });
+
+// })
+// .catch((error) => observer.error(error));
+// });
